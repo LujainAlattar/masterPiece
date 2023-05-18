14a03f0f7dea8f3fdec9eab691fc1ca8a0d8af95
LujainAlattar <LujainA.Alattar@gmail.com>
2023-05-18 22:52:28 +0300

    Your commit message

---
+++ resources/views/layout/masterpage.blade.php
@@ -0,0 +1,3 @@
+@extends('layout.header')
+@yield('content')
+@extends('layout.footer')
---
+++ resources/views/login.blade.php
@@ -0,0 +1,18 @@
+@extends('layout.masterpage')
+@section('content')
+    <div class="col-lg-5 mx-auto" data-aos="fade-up" data-aos-delay="500">
+        <form action="" method="post" class="form-box">
+            <h3 class="h4 text-black mb-4">Login</h3>
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Email Addresss">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" placeholder="Password">
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary btn-pill" value="Login">
+            </div>
+        </form>
+
+    </div>
+@endsection
